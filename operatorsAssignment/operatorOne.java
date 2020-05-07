6204a9198ba8fb3f9b19adbe33a8b7c1f70042ed
Sweta-Selenium <shahsweta310@gmail.com>
2020-05-07 23:02:24 +0530

    adding folder

---
+++ operatorsAssignment/operatorOne.java
@@ -0,0 +1,15 @@
+package operatorsAssignment;
+
+public class operatorOne {
+
+	public static void main(String[] args) {
+		
+
+		
+		int a = 20, b =30;
+        String c = "Selenium", d = " Webdriver";
+		System.out.println(a + b); 
+		System.out.println(c + d);
+	}
+
+}
